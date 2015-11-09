2ad7da09e44ef35a3e45c6c30e352d64a341cf2c
snopeks <stephaniesnopek@gmail.com>
2015-11-08 23:10:31 -0800

    added success tests

--- main.js
+++ main.js
@@ -3,60 +3,53 @@ var request = require('request');
 
 function triggerAlert(event, callback) {
   if (!process.env.PAGER_DUTY_SERVICE) {
-    callback(new Error("Do not have environment variable PAGER_DUTY_SERVICE"))
-    return
+    callback(new Error("Do not have environment variable PAGER_DUTY_SERVICE"));
+    return;
   }
-
-
 
   var payload = {
     'event_type': 'trigger',
     'client': os.hostname(),
-    'service_key': 'PAGER_DUTY_SERVICE'
+    'service_key': process.env.PAGER_DUTY_SERVICE
+  };
+
+  if (typeof event === 'string') {
+    payload.description = event;
+  } else if (typeof event === 'object') {
+    payload.description = event.description;
+    if (event.context && Array.isArray(event.context)) {
+      payload.context = event.context;
+    }
+    if (event.details && event.details === 'object') {
+      payload.details = event.details;
+    }
   }
-  /*
-  payload.event_type = 'trigger'
-  payload.client = os.hostname()
-  payload.service_key = 'PAGER_DUTY_SERVICE'
-  */
+  //make sure the payload has a description field and that description is a string
+  if (!payload.description || typeof payload.description !== 'string') {
+    callback(new Error("Your alert needs a description"));
+    return;
+  }
+
 
   var options = {
     uri: 'https://events.pagerduty.com/generic/2010-04-15/create_event.json',
     method: 'POST',
-    json: {'service_key': 'e93facc04764012d7bfb002500d5d1a6',
-      'event_type': 'trigger',
-      'description': 'FAILURE for production/HTTP on machine srv01.acme.com',
-      'client': 'Sample Monitoring Service'}
-  }
-  //event must be an object OR string
-  //check if event is a string
-  if(typeof event === 'string'){
-    payload.description = event
-  }
-  //check if event is an object
-  else if (typeof event === 'object'){
-      payload.description = event.description
-      if(event.context && Array.isArray(event.context)) {
-        payload.context = event.context
-      }
-      if(event.details && event.details === 'object'){
-        payload.details = event.details
-      }
-  }
-  //if event is not string or object throw an error
-  else {
-    console.log('Error: event type')
-  }
-  console.log(payload)
-  callback(null, "ID")
+    json: payload
+  };
 
-  request(options, function(error, response){
-    callback(error, response.body.incident_key);
-    console.log(response.body)
-  })
-};
+
+  request(options, function(err, response, body) {
+    if(err) {return callback(err);}
+    if(response.statusCode >= 400){
+      return callback(new Error ("Unexpected status code " + response.statusCode ));
+    }
+    callback(err, response.body.incident_key);
+  });
+}
 
 //node convention: check to see if something's wrong at the beginning, and if you don't have the required info then bail with error
+//node convention: err response body. Err means something went horribly wrong communicating to server. if (err) return callback(err);
+//cont'd.. http response. if (res.StatusCode === 200){happy path}. OR
 //assert function:
 //Need to get service_key from the env var!!!
 //need to send payload to pagerduty
--- test.js
+++ test.js
@@ -1,19 +1,62 @@
 var demand = require("must");
 var triggerAlert = require("./main");
 
-describe ("alertbot", function(){
+describe("alertbot", function() {
 
-  it("returns an error if it doesn't have it's environment variable", function(done){
-      var saved = process.env.PAGER_DUTY_SERVICE
-      delete process.env.PAGER_DUTY_SERVICE
+  it("returns an error if it doesn't have it's environment variable", function(done) {
+    var saved = process.env.PAGER_DUTY_SERVICE;
+    delete process.env.PAGER_DUTY_SERVICE;
 
-      triggerAlert({}, function(err){
-        err.must.be.an.object();
-        err.must.match(/environment variable PAGER_DUTY_SERVICE/);
-        process.env.PAGER_DUTY_SERVICE = saved;
-        done()
-      });
+    triggerAlert({}, function(err) {
+      err.must.be.an.object();
+      err.must.match(/environment variable PAGER_DUTY_SERVICE/);
+      process.env.PAGER_DUTY_SERVICE = saved;
+      done();
+    });
 
-  })
+  });
 
-})
+  it("returns an error if it doesn't get a description", function(done) {
+    triggerAlert({}, function(err) {
+      err.must.be.an.object();
+      err.must.match(/description/);
+      done();
+    });
+  });
+
+  it("returns an error if description isn't string", function(done) {
+    triggerAlert(4, function(err) {
+      err.must.be.a.object();
+      err.must.match(/description/);
+      done();
+    });
+  });
+
+  it("successfully triggers pagerduty with string description", function(done) {
+    process.env.PAGER_DUTY_SERVICE = 'e93facc04764012d7bfb002500d5d1a6';
+    triggerAlert("This is a test!", function(err, incident_key) {
+      incident_key.must.be.a.string();
+      done();
+    });
+  });
+  it("successfully triggers pagerduty with event object", function(done) {
+    process.env.PAGER_DUTY_SERVICE = 'e93facc04764012d7bfb002500d5d1a6';
+    var event = {
+       "description": "Testing event object",
+       "contexts":[
+        {
+          "type": "link",
+          "href": "http://acme.pagerduty.com"
+        }],
+        "details": {
+          "ping time": "1500ms",
+          "load avg": 0.75
+        }
+    };
+    triggerAlert(event, function(err, incident_key) {
+      incident_key.must.be.a.string();
+      done();
+    });
+  });
+
+});
