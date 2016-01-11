6da48eb4fbb2e392903407bccd867160b8f6c32a
C J Silverio <ceejceej@gmail.com>
2016-01-11 14:30:38 -0800

    Added a `resolve()` function to resolve alerts.
    There is also a new `pd-resolve` command-line tool that can resolve incidents
    for you. Refactored the module slightly so that the http request is in a utility
    function.
    
    Added the `nyc` code coverage tool to the default test suite.
    Added tests until we're at 100% code coverage.
    Documented the new feature.
    
    Added a license file to make it clear what the module license is.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 `npm install pagerduty-trigger`
 
-A small module that allows you to [trigger](https://developer.pagerduty.com/documentation/integration/events/trigger) a [PagerDuty](https://www.pagerduty.com/) alert and returns an `incident key`.
+A small module that allows you to [trigger](https://developer.pagerduty.com/documentation/integration/events/trigger) and resolve a [PagerDuty](https://www.pagerduty.com/) alerts.
 
 ## Usage
 
@@ -16,9 +16,18 @@ export PAGER_DUTY_API_KEY=an-api-key-with-write-perms
 You can then trigger an alert like this:
 
 ```js
-var triggerAlert = require('pagerduty-trigger');
-triggerAlert('One of the flayrods has gone out of skew on treadle!', function(err, incidentID) {
+var trigger = require('pagerduty-trigger');
+trigger('Trouble at mill!', function(err, incidentID) {
     console.log('incident created with id ' + incidentID);
+});
+```
+
+Resolve the same incident like this:
+
+```js
+var resolve = require('pagerduty-trigger').resolve;
+resolve(incidentID, function(err, response) {
+    console.log('we have now resolved ' + incidentID);
 });
 ```
 
@@ -52,10 +61,16 @@ triggerAlert(event, function(err, incident_key) {
 });
 ```
 
+The `resolve()` function requires either a string incident_key argument or an object with an `incident_key` field. See [PagerDuty's documentation](https://developer.pagerduty.com/documentation/integration/events/resolve) for other fields you might pass.
+
 ## command-line tool
 
 You can create an event using the handy command-line tool `pd-trigger`. Usage:
 
-`pd-trigger "One of the flayrods has gone out of skew on treadle!"`
+`pd-trigger "One of t'flayrods has gone out of skew on treadle!"`
 
-It will respond with the ID of the incident created.
+It will respond with the ID of the incident created. You can then feed that ID to the resolve script: `pd-resolve incident-id`.
+
+## License
+
+ISC.
--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 /*global describe:true, it:true, before:true, after:true, beforeEach: true, afterEach:true */
 
-var demand = require('must');
-var triggerAlert = require('./main');
+var demand = require('must'),
+  request      = require('request'),
+  triggerAlert = require('./main'),
+  resolveAlert = triggerAlert.resolve;
 
 describe('alertbot', function() {
+
+  var savedKey;
+
   it('returns an error if it doesn\'t have its environment variable', function(done) {
     var saved = process.env.PAGER_DUTY_SERVICE;
     delete process.env.PAGER_DUTY_SERVICE;
 
-    triggerAlert({}, function(err) {
+    triggerAlert('description', function(err) {
       err.must.be.an.object();
       err.must.match(/environment variable PAGER_DUTY_SERVICE/);
       process.env.PAGER_DUTY_SERVICE = saved;
@@ -20,7 +25,7 @@ describe('alertbot', function() {
     var saved = process.env.PAGER_DUTY_API_KEY;
     delete process.env.PAGER_DUTY_API_KEY;
 
-    triggerAlert({}, function(err) {
+    triggerAlert('description', function(err) {
       err.must.be.an.object();
       err.must.match(/environment variable PAGER_DUTY_API_KEY/);
       process.env.PAGER_DUTY_API_KEY = saved;
@@ -44,6 +49,18 @@ describe('alertbot', function() {
     });
   });
 
+  it('responds with an error if the auth is incorrect', function(done) {
+    process.env.PAGER_DUTY_SERVICE = 'deadbeef';
+    var saved = process.env.PAGER_DUTY_API_KEY;
+    process.env.PAGER_DUTY_API_KEY = 'deadbeef';
+    triggerAlert('This is a test!', function(err, incident_key) {
+      err.must.be.a.object();
+      err.must.match(/Unexpected status code/);
+      process.env.PAGER_DUTY_API_KEY = saved;
+      done();
+    });
+  });
+
   it('successfully triggers pagerduty with string description', function(done) {
     process.env.PAGER_DUTY_SERVICE = 'e93facc04764012d7bfb002500d5d1a6';
     triggerAlert('This is a test!', function(err, incident_key) {
@@ -56,7 +73,7 @@ describe('alertbot', function() {
     process.env.PAGER_DUTY_SERVICE = 'e93facc04764012d7bfb002500d5d1a6';
     var event = {
        'description': 'Testing event object',
-       'contexts':[
+       'context':[
         {
           'type': 'link',
           'href': 'http://acme.pagerduty.com'
@@ -69,8 +86,44 @@ describe('alertbot', function() {
     triggerAlert(event, function(err, incident_key) {
       demand(err).not.exist();
       incident_key.must.be.a.string();
+
+      savedKey = incident_key;
       done();
     });
   });
 
+  it('responds with an error when no incident key is passed', function(done) {
+    process.env.PAGER_DUTY_SERVICE = 'e93facc04764012d7bfb002500d5d1a6';
+    resolveAlert({}, function(err, incident_key) {
+      err.must.exist();
+      err.must.match(/incident_key/);
+      done();
+    });
+  });
+
+  it('successfully resolves alert with an id', function(done) {
+    this.timeout = 4000;
+    process.env.PAGER_DUTY_SERVICE = 'e93facc04764012d7bfb002500d5d1a6';
+    resolveAlert(savedKey, function(err, response) {
+      demand(err).not.exist();
+      response.must.be.an.object();
+      response.must.have.property('incident_key');
+      savedKey.must.equal(response.incident_key);
+      response.status.must.equal('success');
+      done();
+    });
+  });
+
+  it('successfully resolves an alert with a details object', function(done) {
+    this.timeout = 4000;
+    process.env.PAGER_DUTY_SERVICE = 'e93facc04764012d7bfb002500d5d1a6';
+    resolveAlert({ incident_key: 'asdf', }, function(err, response) {
+      demand(err).not.exist();
+      response.must.be.an.object();
+      response.must.have.property('incident_key');
+      response.incident_key.must.equal('asdf');
+      response.status.must.equal('success');
+      done();
+    });
+  });
 });
